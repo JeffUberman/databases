541fef345e718d1b03d652c3ebe231fd10be4e00
MKS-students <mks.students@hackreactor.com>
2015-08-21 19:56:05 -0500

    Client imported

--- orm-resources/orm-example.js
+++ orm-resources/orm-example.js
@@ -4,21 +4,26 @@
  */
 
 var Sequelize = require("sequelize");
-var sequelize = new Sequelize("chatter", "root", "");
+var sequelize = new Sequelize("chats", "root", "taco");
 /* TODO this constructor takes the database name, username, then password.
  * Modify the arguments if you need to */
 
 /* first define the data structure by giving property names and datatypes
  * See http://sequelizejs.com for other datatypes you can use besides STRING. */
-var User = sequelize.define('User', {
-  username: Sequelize.STRING
+var Users = sequelize.define('users', {
+  username: Sequelize.STRING,
+  password: Sequelize.STRING
 });
 
-var Message = sequelize.define('Message' {
-  userid: Sequelize.INTEGER,
-  text: Sequelize.STRING,
-  roomname: Sequelize.STRING
+var Messages = sequelize.define('messages', {
+  user_id: Sequelize.INTEGER,
+  message: Sequelize.STRING,
+  room_id: Sequelize.INTEGER
 });
+
+var Rooms = sequelize.define('rooms', {
+  roomname: Sequelize.STRING
+})
 
 /* .sync() makes Sequelize create the database table for us if it doesn't
  *  exist already: */
